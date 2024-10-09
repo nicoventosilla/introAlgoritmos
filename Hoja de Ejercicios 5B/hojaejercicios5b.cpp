a9555de91793b74997c78d47817f036584434814
Nico <86941298+nicoventosilla@users.noreply.github.com>
2024-10-09 18:14:29 -0500

    problema2 5B complete

--- Hoja de Ejercicios 5B/hojaejercicios5b.cpp
+++ Hoja de Ejercicios 5B/hojaejercicios5b.cpp
@@ -4,7 +4,7 @@
 
 #include "hojaejercicios5b.h"
 #include "problema1.h"
-// #include "problema2.h"
+#include "problema2.h"
 // #include "problema3.h"
 // #include "problema4.h"
 // #include "problema5.h"
@@ -19,7 +19,7 @@ using namespace std;
 void mostrarMenu()
 {
     cout << "1. Problema 1" << endl;
-    // cout << "2. Problema 2" << endl;
+    cout << "2. Problema 2" << endl;
     // cout << "3. Problema 3" << endl;
     // cout << "4. Problema 4" << endl;
     // cout << "5. Problema 5" << endl;
@@ -45,9 +45,9 @@ int main()
     case 1:
         problema1();
         break;
-    // case 2:
-    //     problema2();
-    //     break;
+    case 2:
+        problema2();
+        break;
     // case 3:
     //     problema3();
     //     break;
---
+++ Hoja de Ejercicios 5B/problema2.h
@@ -0,0 +1,14 @@
+//
+// Created by c0d3r on 09/10/2024.
+//
+
+#ifndef PROBLEMA2_H
+#define PROBLEMA2_H
+
+
+
+void problema2();
+
+
+
+#endif //PROBLEMA2_H
