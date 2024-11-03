f55dc212294b5b87efbb3cb5e074e2be5e9ae784
Nico <86941298+nicoventosilla@users.noreply.github.com>
2024-11-03 16:50:49 -0500

    problema4 HojaEjercicios10 completo

--- Hoja de Ejercicios 10 - Arreglos unidimensionales/hojaejercicios10.cpp
+++ Hoja de Ejercicios 10 - Arreglos unidimensionales/hojaejercicios10.cpp
@@ -6,7 +6,7 @@
 #include "problema1.h"
 #include "problema2.h"
 #include "problema3.h"
-// #include "problema4.h"
+#include "problema4.h"
 // #include "problema5.h"
 #include <iostream>
 using namespace std;
@@ -16,7 +16,7 @@ void mostrarMenu()
     cout << "1. Problema 1" << endl;
     cout << "2. Problema 2" << endl;
     cout << "3. Problema 3" << endl;
-    // cout << "4. Problema 4" << endl;
+    cout << "4. Problema 4" << endl;
     // cout << "5. Problema 5" << endl;
 }
 
@@ -40,9 +40,9 @@ int main()
     case 3:
         problema3();
         break;
-    // case 4:
-    //     problema4();
-    //     break;
+    case 4:
+        problema4();
+        break;
     // case 5:
     //     problema5();
     //     break;
--- Hoja de Ejercicios 10 - Arreglos unidimensionales/problema4.cpp
+++ Hoja de Ejercicios 10 - Arreglos unidimensionales/problema4.cpp
@@ -4,10 +4,89 @@
 
 #include "problema4.h"
 #include <iostream>
+#include <cstdlib> // Para usar srand y rand
+#include <ctime> // Para usar time
 
 using namespace std;
 
 void problema4()
 {
+    srand(time(0));
 
+    int numero_pernos;
+
+    do
+    {
+        cout << "Ingrese el numero de pernos: ";
+        cin >> numero_pernos;
+
+        if (numero_pernos < 1 || numero_pernos > 100)
+        {
+            cout << "El numero de pernos debe estar entre 1 y 100" << endl;
+        }
+    }
+    while (numero_pernos < 1 || numero_pernos > 100);
+
+    int* Pernos = new int[numero_pernos];
+
+    // Genera un numero aleatorio entre 5 y 30
+    for (int i = 0; i < numero_pernos; i++)
+    {
+        Pernos[i] = rand() % 26 + 5;
+    }
+
+    // Ordenar el diametro de los pernos de mayor a menor
+    for (int j = 0; j < numero_pernos - 1; j++)
+    {
+        for (int i = j + 1; i < numero_pernos; i++)
+        {
+            if (Pernos[j] < Pernos[i])
+            {
+                int aux = Pernos[j];
+                Pernos[j] = Pernos[i];
+                Pernos[i] = aux;
+            }
+        }
+    }
+
+    cout << "Diametro de los pernos ordenados de mayor a menor: " << endl;
+
+    for (int i = 0; i < numero_pernos; i++)
+    {
+        cout << Pernos[i] << " ";
+    }
+
+    // Calcula el promedio de los pernos
+    double promedio = 0.0;
+
+    for (int i = 0; i < numero_pernos; i++)
+    {
+        promedio += Pernos[i];
+    }
+
+    promedio /= numero_pernos;
+
+    cout << "\nEl promedio del diametro de los pernos es: " << promedio << endl;
+
+    int diametro;
+    cout << "Ingrese el diametro del perno a buscar: ";
+    cin >> diametro;
+
+    int cantidad_pernos_inferiores = 0;
+
+    for (int i = 0; i < numero_pernos; i++)
+    {
+        if (Pernos[i] < diametro)
+        {
+            cantidad_pernos_inferiores++;
+        }
+    }
+
+    cout << "Cantidad de pernos con diametro menor a " << diametro << ": " << cantidad_pernos_inferiores << endl;
+
+    // si más del 50% de los pernos posee un diámetro inferior a ingresado imprima el mensaje "Lote defectuoso"
+    if (cantidad_pernos_inferiores > numero_pernos / 2)
+    {
+        cout << "Lote defectuoso" << endl;
+    }
 }
